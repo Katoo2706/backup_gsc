1b1ba45a00ada60ed3445605ef2f5a0c04eebd9f
katoo2706 <quan.ngo@advesa.com>
2023-09-07 00:17:19 +0700

    revise for crontab using

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# shellcheck disable=SC2164
+cd '/Users/kato/Desktop/PRODUCTION/analytic-database-backup'
+
 # Load environment variables
 source ./backup/.env
 
@@ -14,16 +17,20 @@ export PGPASSWORD="$PGPASSWORD"
 
 # Perform the database backup
 mkdir -p ./backup/temp
+
+echo "Dumping file: Please wait a while ======="
+echo "..."
 pg_dump -h "$PGHOST" -p "$PGPORT" -U "$PGUSER" "$PGDATABASE" | gzip > ./backup/temp/"$BACKUP_FILE"
 
+echo "Gzip data to temp folder"
 # Back up processing
-if gsutil --impersonate-service-account="$IMPERSONATE_SERVICE_ACCOUNT"\
+if gsutil --impersonate-service-account="$IMPERSONATE_SERVICE_ACCOUNT" \
   cp ./backup/temp/"$BACKUP_FILE" gs://"$GCS_BUCKET"/"$GCS_BACKUP_PATH"/; then
-    echo "Successfully back up to Google Cloud Storage"
+    echo "Successfully backed up to Google Cloud Storage"
     echo "gs://advesa_data_psql_backup/backup/$BACKUP_FILE" >> ./log.txt
 else
     echo "Backup failed."
 fi
 
-# Delete old backup files
-rm ./backup/temp/*
+## Delete old backup files
+#rm ./backup/temp/*
